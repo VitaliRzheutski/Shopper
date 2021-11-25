ed87f1ceadc4d66963a1c80e01e7261328c6f288
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2021-11-24 23:03:08 -0500

    db is created

--- server/db/index.js
+++ server/db/index.js
@@ -2,18 +2,41 @@
 // with your models, for which you'll find some blank files in this directory:
 
 const db = require('./database')
-const Student = require('./student')
-const Campus = require('./campus')
+const User = require('./user')
+const Order = require('./order')
+const Product = require('./product')
+const orderDetail = require('./orderDetails')
 
 // This is a great place to establish associations between your models
 // (https://sequelize-guides.netlify.com/association-types/).
 // Example:
 //
 // Puppy.belongsTo(Owner)
+User.hasMany(Order);
+Order.belongsTo(User);
+
+
+Product.belongsToMany(Order,{
+  through:{
+    model:'orderDetail',
+    as:'productId',
+    unique:false
+  }
+});
+Order.belongsToMany(Product, {
+  through: {
+    model: 'orderDetail',
+    as: 'orderId',
+    unique: false
+  }
+})
 
 module.exports = {
   // Include your models in this exports object as well!
   db,
-  Student,
-  Campus,
+  Product,
+  User,
+  Order,
+  orderDetail
+  
 }
