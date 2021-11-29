c92b8e773a7cf7e9ebe046d8157959e08e01f0f3
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2021-11-29 17:20:25 -0500

    created redux for AllProducts

--- app/components/AllProducts.js
+++ app/components/AllProducts.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchProductsThunk } from "../redux/products";
 
 // Notice that we're exporting the AllCampuses component twice. The named export
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
 export class AllCampuses extends React.Component {
+  componentDidMount(){
+    this.props.loadProducts()
+  }
   render() {
-    return <div />;
+    // console.log('this.props:',this.props)
+    return <div>hI!</div>;
   }
 }
 
-const mapState = () => {
-  return {};
+const mapState = (state) => {
+  return {
+    products: state.products
+  };
 };
 
-const mapDispatch = () => {
-  return {};
+const mapDispatch = (dispatch) => {
+  return {
+    loadProducts:() =>dispatch(fetchProductsThunk())
+  };
 };
 
 export default connect(mapState, mapDispatch)(AllCampuses);
